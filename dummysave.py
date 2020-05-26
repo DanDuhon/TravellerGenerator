ad57ca14fc3684deb341251d31b3138718982012
anythingapplied <anythingjewish@gmail.com>
2020-05-26 18:09:14 -0500

    dummy save testing

--- dummysave.py
+++ dummysave.py
@@ -1,37 +1,38 @@
+import pickle
 import random
 import json
 
 
 class System:
     def __init__(self):
+        self.property_list = ['starcount', 'stars']
         self.starcount = random.randint(1,3)
         self.stars = []
         for x in range(self.starcount):
             self.stars.append(Star())
-        self.property_list = ['starcount', 'stars']
 
     def save(self, filename):
-        save_dict = {key:self[key] for key in self.property_list}
+        save_dict = {key:getattr(self, key) for key in self.property_list}
         with open(filename, 'w') as f:
             json.dump(save_dict, f, sort_keys=True, default=lambda x: x.save())
 
     def load(self, filename):
-        print("sc1", self.get('starcount'))
+        print("sc1", getattr(self, 'starcount'))
         for key in self.property_list:
-            del self[key]
-        print("sc2", self.get('starcount'))
+            delattr(self, key)
+        print("sc2", getattr(self, 'starcount', None))
         with open(filename, 'r') as f:
             loaded = json.load(f)
-        self.starcount = loaded['starcount']
-        self.stars = [Star(load=star) for star in loaded['stars']]
-        print("sc3", self.get('starcount'))
+        for key in self.property_list:
+            setattr(self, key, loaded[key])
+        print("sc3", getattr(self, 'starcount'))
 
 class Star:
     def __init__(self, loaded=None):
+        self.property_list = ['lumin', 'planets']
         if loaded:
-            self.lumin = loaded['lumin']
-            self.planets = [Planet(star=self, loaded=planet) for planet in
-                    loaded['planets'])
+            for key in self.property_list:
+                setattr(self, key, loaded[key])
             return
         self.lumin = 15
         self.planets = []
@@ -42,23 +43,22 @@ class Star:
         print(lumin)
 
     def save(self):
-        save_dict = {}
-        save_dict['lumin'] = self.lumin
-        save_dict['planets'] = self.planets
+        save_dict = {key:getattr(self, key) for key in self.property_list}
+        save_dict['save_type'] = 'star'
         return save_dict
 
 class Planet:
     def __init__(self, star=None, loaded=None):
+        self.property_list = ['size']
         if loaded:
-            self.size = loaded['size']
-            self.star = star
-            return
+            for key in self.property_list:
+                setattr(self, key, loaded[key])
         self.size = random.randint(1,5)
         self.star = star
 
     def save(self):
-        save_dict = {}
-        save_dict['size'] = self.size
+        save_dict = {key:getattr(self, key) for key in self.property_list}
+        save_dict['save_type'] = 'planet'
         return save_dict
 
 system = System()
@@ -67,3 +67,4 @@ system = System()
 system.save('file2.trav')
 system.load('file1.trav')
 system.save('file3.trav')
+pickle.dump(system, open('pfile1.trav', "wb"), protocol=0)
